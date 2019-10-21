cb03b84a5c54c7677037e4ffce1026325b97198d
Mel Maki <melody.rackham@gmail.com>
2019-10-21 16:07:42 -0600

    Styled Profile Page using styled components and created value cards

---
+++ my-app/src/components/Card.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Card, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import styled from 'styled-components';
+
+const ValueCard = styled.p`
+  font-size: 2rem;
+  font-family: 'Liu Jian Mao Cao', cursive;
+  border: 2px dotted #624f69;
+  width: 25%;
+  background-color: #a4afcc;
+  padding-bottom: 30px;
+`;
+
+const Title = styled.h2`
+  color: black;
+  font-size: 3rem;
+  font-family: 'Liu Jian Mao Cao', cursive;
+`;
+
+const ValueCards = props => {
+  return (
+    <div className='ValueCard'>
+      <ValueCard>
+        <Card>
+          <CardBody>
+            <Title>
+              <CardTitle> Kindness </CardTitle>
+            </Title>
+            <CardSubtitle> "Kindness is seeing the best in others when they cannot see it in themselves" </CardSubtitle>
+            <Button>Add to my profile</Button>
+          </CardBody>
+        </Card>
+      </ValueCard>
+    </div>
+  );
+};
+
+export default ValueCards;
--- my-app/src/components/ProfilePage.js
+++ my-app/src/components/ProfilePage.js
@@ -1,18 +1,63 @@
 import React from 'react';
 import styled from 'styled-components';
-
-const Welcome = styled.h1`
-  color: purple;
-  font-size: 4rem;
-  font-family: 'Long Cang', cursive;
-`;
+import Card from './Card';
 
 export default function ProfilePage() {
   return (
     <section className='profile-page'>
       <header>
-        <Welcome>Welcome to the PROFILE PAGE! </Welcome>
+        <Welcome> My Profile </Welcome>
+        <NameOne> First Name </NameOne>
+        <NameTwo> Last Name </NameTwo>
+        <Email> Email: </Email>
+        <Bio> Bio: </Bio>
+        <Essentials> The Essentials in my life: </Essentials>
       </header>
+      <Card />
     </section>
   );
 }
+
+/// styled components:
+
+const Welcome = styled.h1`
+  color: purple;
+  font-size: 4rem;
+  font-family: 'Liu Jian Mao Cao', cursive;
+`;
+
+const NameOne = styled.p`
+  font-size: 2rem;
+  color: black;
+  font-family: 'Liu Jian Mao Cao', cursive;
+`;
+
+const NameTwo = styled.p`
+  font-size: 2rem;
+  color: black;
+  font-family: 'Liu Jian Mao Cao', cursive;
+`;
+
+const Email = styled.p`
+  font-size: 2rem;
+  color: black;
+  font-family: 'Liu Jian Mao Cao', cursive;
+`;
+
+const Bio = styled.p`
+  font-size: 2rem;
+  color: black;
+  font-family: 'Liu Jian Mao Cao', cursive;
+`;
+
+const Essentials = styled.p`
+  font-size: 2rem;
+  color: black;
+  font-family: 'Liu Jian Mao Cao', cursive;
+`;
+
+const CardOne = styled.p`
+  font-size: 2rem;
+  color: black;
+  font-family: 'Liu Jian Mao Cao', cursive;
+`;
