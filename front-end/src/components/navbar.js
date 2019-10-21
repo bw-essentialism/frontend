edc7d340ba688218fd1fa831da92cd6c7bd09ed5
aaamg <andrewmanuelgarcia@gmail.com>
2019-10-21 16:23:19 -0400

    navbar added

--- front-end/src/components/navbar.js
+++ front-end/src/components/navbar.js
@@ -6,11 +6,11 @@ import * as ReactBootstrap from 'react-bootstrap';
 export default function Navigation () {
     return (
         <div>
-        <Navbar bg="dark" expand="lg">
+        <Navbar bg="dark"  variant="dark" expand="lg">
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
             <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="/">Log In</Nav.Link>
+            <Nav.Link href="/register">Register</Nav.Link>
             </Nav>
         </Navbar>
         </div>
